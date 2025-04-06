ed9293bf10fabcf0a06ca49348d230a372158496
Anamul-Haque <md.helpoperation@gmail.com>
2025-04-06 08:31:53 +0600

    creat Quta home work

---
+++ src/RandDomQuta/quta.js
@@ -0,0 +1,32 @@
+window.onload()
+
+const qutaTitle = [
+    {
+        Quta: 'some text input here',
+        Autor: 'autor Name Here',
+    },
+    {
+        Quta: 'some text input here 2',
+        Autor: 'autor Name Here 2',
+    },
+    {
+        Quta: 'some text input here 3 ',
+        Autor: 'autor Name Here 3',
+    },
+    {
+        Quta: 'some text input here 4',
+        Autor: 'autor Name Here 4',
+    },
+    {
+        Quta: 'some text input here 5',
+        Autor: 'autor Name Here 5',
+    },
+];
+
+const qutaTitel = document.getElementById('qutaTitle')
+const authorName = document.getElementById('authorName')
+const addButton = document.getElementById('addButton')
+
+function qutaArrange(){
+    const qutaRand = Math.floor(Math.random) * qutaTitel.
+}
